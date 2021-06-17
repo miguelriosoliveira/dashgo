5ee415e632af226ad978742306ad768065472968
Miguel Rios <miguelriosoliveira@gmail.com>
2021-06-17 20:32:53 -0300

    WIP: creating header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Flex } from '@chakra-ui/react';
+
+export function Header() {
+	return (
+		<Flex as="header" w="100%" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
+			asds
+		</Flex>
+	);
+}
