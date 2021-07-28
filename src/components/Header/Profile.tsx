be28a15b0f9d135a95be8ae7161212b705374c46
Miguel Rios <miguelriosoliveira@gmail.com>
2021-07-27 23:29:29 -0300

    Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+	hideProfileData?: boolean;
+}
+
+export function Profile({ hideProfileData = false }: ProfileProps) {
 	return (
 		<Flex align="center">
-			<Box mr="4" textAlign="right">
-				<Text>Miguel Rios</Text>
-				<Text color="gray.300" fontSize="small">
-					miguelriosoliveira@gmail.com
-				</Text>
-			</Box>
+			{!hideProfileData && (
+				<Box mr="4" textAlign="right">
+					<Text>Miguel Rios</Text>
+					<Text color="gray.300" fontSize="small">
+						miguelriosoliveira@gmail.com
+					</Text>
+				</Box>
+			)}
 
 			<Avatar size="md" name="Miguel Rios" src="https://github.com/miguelriosoliveira.png" />
 		</Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -6,14 +6,16 @@ import { Profile } from './Profile';
 import { SearchBar } from './SearchBar';
 
 export function Header() {
+	const isWideScreen = useBreakpointValue({ base: false, lg: true });
+
 	return (
 		<Flex as="header" w="100%" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
 			<Logo />
-			<SearchBar />
+			{isWideScreen && <SearchBar />}
 
 			<Flex align="center" ml="auto">
 				<NotificationsNav />
-				<Profile />
+				<Profile hideProfileData={!isWideScreen} />
 			</Flex>
 		</Flex>
 	);
