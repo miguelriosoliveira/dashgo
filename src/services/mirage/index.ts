5c6e93b946c17cdf9ffbc374728a89186e6331db
Miguel Rios <miguelriosoliveira@gmail.com>
2021-10-26 21:16:10 -0300

    feature: Adding pagination to Mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,5 @@
 import faker from 'faker';
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 
 interface User {
 	name: string;
@@ -36,7 +36,22 @@ export function makeServer() {
 			this.namespace = 'api';
 			this.timing = 750;
 
-			this.get('/users');
+			this.get('/users', function f(schema, request) {
+				const { page = 1, per_page = 10 } = request.queryParams;
+				const total = schema.all('user').length;
+				const pageStart = (Number(page) - 1) * Number(per_page);
+				const pageEnd = pageStart + Number(per_page);
+				const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd);
+				return new Response(
+					200,
+					{
+						'x-total-count': String(total),
+					},
+					{
+						users,
+					},
+				);
+			});
 			this.post('/users');
 
 			this.namespace = '';
